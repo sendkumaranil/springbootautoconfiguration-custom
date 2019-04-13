249df5e7b90ec35ef760fa8ffb9528ebb95ced7f
Anil Kumar <anilkumar@Anils-MacBook-Air.local>
2019-04-13 13:49:23 +0530

    Added Project

---
+++ greeting-library/src/main/java/com/greeting/library/GreetingConfig.java
@@ -0,0 +1,9 @@
+package com.greeting.library;
+
+import java.util.Properties;
+
+public class GreetingConfig extends Properties{
+    
+    private static final long serialVersionUID = 5662570853707247891L;
+
+}
